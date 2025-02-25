ea6ed2f2c410cd3d25c12f396366655b93128a5f
murac18 <shamura.cargo@carvercollegiate.org>
2025-02-25 19:46:16 +0000

    platformer

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -27,24 +27,41 @@ $(function () {
     //////////////////////////////////
 
     // TODO 1 - Enable the Grid
-    // toggleGrid();
+    toggleGrid();
 
 
     // TODO 2 - Create Platforms
+    createPlatform(300,600,100,20);
+    createPlatform(500,500,100,20);
+    createPlatform(100,300,100,20);
+    createPlatform(600,200,100,20);
+    createPlatform(900,100,100,20);
+    createPlatform(300,400,100,20);
+    createPlatform(300,200,100,20);
+    createPlatform(200,700,100,20);
+    createPlatform(800,560,100,20);
+
+
+
 
 
 
 
     // TODO 3 - Create Collectables
-
+   createCollectable("grace", 300,100, 0, 0.7);
+   createCollectable("diamond", 300,300, 0, 0.7);
+   createCollectable("kennedi", 100,100, 0.8, 0.7);
+   createCollectable("database", 600,100, 0, 0.7);
+   createCollectable("database", 900,0, 0, 0.7);
 
 
     
     // TODO 4 - Create Cannons
-
-
-    
-    
+    createCannon("bottom", 1300, 900);
+    createCannon("right", 300, 2000);
+    createCannon("left", 300, 1900);
+    createCannon("top", 800,700)
+      
     //////////////////////////////////
     // ONLY CHANGE ABOVE THIS POINT //
     //////////////////////////////////
